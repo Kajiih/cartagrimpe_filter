2ca77c7d38e251b1345de36ecefbf187e41db400
Julian Paquerot <111422357+Kajiih@users.noreply.github.com>
2024-12-04 17:55:45 +0100

    🚧 Continue script

--- src/cartagrimpe_filter/cartagrimpe_filter.py
+++ src/cartagrimpe_filter/cartagrimpe_filter.py
@@ -5,10 +5,10 @@ from __future__ import annotations
 import re
 import sys
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from enum import StrEnum, auto
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, NamedTuple, cast, reveal_type
 from urllib.parse import urljoin
 
 import attrs
@@ -17,15 +17,17 @@ import numpy as np
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup, ResultSet, Tag
-from geopy.distance import geodesic
 from geopy.geocoders import Nominatim
 from loguru import logger
+from requests import HTTPError, RequestException
 
 if TYPE_CHECKING:
+    from collections.abc import Iterator, Sequence
+
     from geopy import Location
 
 REQUEST_TIMEOUT = 10
-URL = "https://cartagrimpe.fr/en/calendrier"
+EVENT_CALENDAR_URL = "https://cartagrimpe.fr/en/calendrier"
 GEOCODE_CACHE_FILE = Path("geocode_cache.json5")
 
 
@@ -36,18 +38,21 @@ class EventType(StrEnum):
     COMPETITION = "compétition"
     FILM = auto()
     GATHERING = "rassemblement"
-    OTHER = auto()
+    OTHER = "autre"
 
 
-@attrs.frozen
+# @attrs.frozen
+@attrs.define
 class Event:
     """An event."""
 
-    name: str
+    name: str = attrs.field()
     url: str
     address: str
-    date_start: float
-    date_end: float
+    date_start: str
+    date_end: str
+    timestamp_start: float
+    timestamp_end: float
     type: EventType
 
     @classmethod
@@ -74,22 +79,21 @@ class Event:
         date_text = tag.find(string=re.compile(r"From", re.IGNORECASE))
         # Example format: "From 14/01/2024 to 14/01/2024"
         date_parts = date_text.replace("From", "").split("to") if date_text else ""  # pyright: ignore[reportOptionalCall, reportAttributeAccessIssue]
-        start_date_str = date_parts[0].strip()
-        end_date_str = date_parts[1].strip() if len(date_parts) > 1 else start_date_str
+        date_start = date_parts[0].strip()
+        date_end = date_parts[1].strip() if len(date_parts) > 1 else date_start
 
-        # Define the date format based on the example
         date_format = "%d/%m/%Y"
-
-        # Parse start and end dates into datetime objects
-        date_start_ts = datetime.strptime(start_date_str, date_format).timestamp()
-        date_end_ts = datetime.strptime(end_date_str, date_format).timestamp()
+        timestamp_start = datetime.strptime(date_start, date_format).timestamp()
+        timestamp_end = datetime.strptime(date_end, date_format).timestamp()
 
         return cls(
             name=name,
             url=url,  # pyright: ignore[reportArgumentType]
             address=address,
-            date_start=date_start_ts,
-            date_end=date_end_ts,
+            date_start=date_start,
+            date_end=date_end,
+            timestamp_start=timestamp_start,
+            timestamp_end=timestamp_end,
             type=event_type,
         )
 
@@ -115,18 +119,21 @@ def get_next_page_url(soup: BeautifulSoup) -> str | None:
     return next_link  # pyright: ignore[reportReturnType]
 
 
-def scrap_events(initial_url: str, nb_pages: int | None = None) -> list[Event]:
+def scrap_events(
+    initial_url: str, start_date: str = "", nb_pages: int | None = None
+) -> list[Event]:
     """Scrap all event divs from the paginated HTML content starting from the initial URL."""
     all_events: list[Event] = []
-    current_url = initial_url
+    current_url = urljoin(initial_url, f"?page=1&date_debut={start_date}")
+
     page_number = 1
 
     while current_url and (nb_pages is None or nb_pages - page_number >= 0):
         logger.debug(f"Fetching page {page_number}: {current_url}")
-        response = requests.get(current_url, timeout=REQUEST_TIMEOUT)
         try:
+            response = requests.get(current_url, timeout=REQUEST_TIMEOUT)
             response.raise_for_status()
-        except requests.exceptions.HTTPError:
+        except (RequestException, HTTPError):
             logger.exception(f"Failed to fetch page {current_url}")
             break
         html_content = response.text
@@ -144,7 +151,11 @@ def scrap_events(initial_url: str, nb_pages: int | None = None) -> list[Event]:
         logger.debug(f"Extracted {len(event_divs)} events from page {page_number}.")
 
         next_page_url = get_next_page_url(soup)
-        current_url = urljoin(current_url, next_page_url) if next_page_url else None
+        current_url = (
+            urljoin(current_url, next_page_url + f"&date_debut={start_date}")
+            if next_page_url
+            else None
+        )
         page_number += 1
 
     return all_events
@@ -155,7 +166,7 @@ def get_current_location() -> Coordinate | None:
     try:
         response = requests.get("http://ip-api.com/json/", timeout=REQUEST_TIMEOUT)
         response.raise_for_status()
-    except (requests.RequestException, requests.exceptions.HTTPError):
+    except (RequestException, HTTPError):
         logger.exception(f"Error fetching IP Geolocation")
         return None
 
@@ -189,13 +200,31 @@ def normalize_address(address: str) -> str:
     return address
 
 
+def get_city[T](location: Location, default: T = None) -> str | T:
+    """Return the city or state/country if city is unavailable."""
+    raw_address = location.raw["address"]
+    keys = ["city", "town", "municipality", "village", "state", "country"]
+
+    # Return the first non-None value from raw_address
+    return next((raw_address.get(key) for key in keys if raw_address.get(key) is not None), default)
+
+
+def get_amenity[T](location: Location, default: T = None) -> str | T:
+    """Return the amenity or house number if city is unavailable."""
+    raw_address = location.raw["address"]
+    keys = ["amenity", "house_number"]
+
+    # Return the first non-None value from raw_address
+    return next((raw_address.get(key) for key in keys if raw_address.get(key) is not None), default)
+
+
 def find_address(coordinate: Coordinate, geolocator: Nominatim) -> str:
     """Return the address string given the coordinate."""
     location: Location = geolocator.reverse(coordinate)  # pyright: ignore[reportAssignmentType]
 
     def format_address(location: Location) -> str:
         address = location.raw["address"]
-        return f"{address['amenity']}, {address['road']}, {address['town']}, {address['country']}"
+        return f"{get_amenity(location)}, {address['road']}, {get_city(location)}, {address['country']}"
 
     return format_address(location)
 
@@ -226,22 +255,29 @@ def save_geocode_cache(cache: dict) -> None:
     logger.debug(f"Geocode cache saved to {GEOCODE_CACHE_FILE}.")
 
 
+class CoordTown(NamedTuple):
+    """Tuple containing coordinate and the town they represent."""
+
+    coords: Coordinate
+    city: str
+
+
 # TODO? Add failed geocoding to cache ?
 def geocode_address(
-    address: str, geolocator: Nominatim, cache: dict[str, Coordinate]
-) -> Coordinate | None:
+    address: str, geolocator: Nominatim, cache: dict[str, CoordTown]
+) -> CoordTown | None:
     """Geocode the given address and return the coordinate with a persistent cache."""
     address = normalize_address(address)
     # Check cache
     cached_address = address.lower()
     if hit := cache.get(cached_address):
         logger.debug(f"Address '{address}' found in cache.")
-        return hit
+        return CoordTown(hit[0], hit[1])
 
     # Geocode
     logger.warning(f"Address '{address}' not found in cache.")
     try:
-        location: Location = geolocator.geocode(address)  # pyright: ignore[reportAssignmentType]
+        location: Location = geolocator.geocode(address, addressdetails=True)  # pyright: ignore[reportAssignmentType]
     except Exception:
         logger.exception(f"Exception during geocoding address '{address}'")
         return None
@@ -251,10 +287,16 @@ def geocode_address(
         return None
 
     coords = (location.latitude, location.longitude)
-    logger.info(f"Geocoded '{address}' to {coords}")
-    cache[cached_address] = coords
 
-    return (location.latitude, location.longitude)
+    city = get_city(location)
+    if city is None:
+        print("NO")
+    assert city is not None
+
+    logger.debug(f"Geocoded '{address}' to {coords}")
+    cache[cached_address] = CoordTown(coords, city)
+
+    return CoordTown((location.latitude, location.longitude), city)
 
 
 def add_geocodes(df: pd.DataFrame, geolocator: Nominatim, cache: dict) -> pd.DataFrame:
@@ -271,39 +313,125 @@ def add_geocodes(df: pd.DataFrame, geolocator: Nominatim, cache: dict) -> pd.Dat
     """
     latitudes = []
     longitudes = []
+    cities = []
     for address in df["address"]:
-        if address != "N/A" and (coords := geocode_address(address, geolocator, cache)):
+        if address != "N/A" and (location := geocode_address(address, geolocator, cache)):
+            coords = location.coords
             latitudes.append(coords[0])
             longitudes.append(coords[1])
+            cities.append(location.city)
         else:
             latitudes.append(None)
             longitudes.append(None)
+            cities.append(None)
     df["latitude"] = latitudes
     df["longitude"] = longitudes
+    df["Ville"] = cities
 
     return df
 
 
-def compute_distances(df: pd.DataFrame, base_coords: Coordinate) -> pd.DataFrame:
-    """Compute distances from base_coords to each event and add as a new column."""
-    distances = []
-    for _, row in df.iterrows():
-        event_coords = (row["latitude"], row["longitude"])
-        if not any(np.isnan(event_coords)):
-            distance = geodesic(base_coords, event_coords).kilometers
-            distances.append(distance)
-        else:
-            distances.append(None)
-    df["distance_km"] = distances
+class TableMatrices(NamedTuple):
+    """Tuple of distance and duration matrices."""
+
+    distance: np.ndarray
+    duration: np.ndarray
+
+
+def get_table_matrices(  # noqa: PLR0914
+    sources: list[Coordinate], destinations: list[Coordinate]
+) -> TableMatrices:
+    """Request distance and duration matrix from the OSRM API."""
+    OSRM_API_URL = "http://router.project-osrm.org/table/v1/driving"
+    BATCH_SIZE = 250
+
+    def batch[T: Sequence](seq: T, n: int) -> Iterator[T]:
+        l = len(seq)
+        for ndx in range(0, l, n):
+            yield cast("T", seq[ndx : min(ndx + n, l)])
+
+    distance_matrix = np.full((len(sources), len(destinations)), None)
+    duration_matrix = np.full((len(sources), len(destinations)), None)
+
+    i = 0
+    for source_batch in batch(sources, BATCH_SIZE):
+        nb_sources = len(source_batch)
+        j = 0
+        for destination_batch in batch(destinations, BATCH_SIZE - nb_sources + 1):
+            nb_destinations = len(destination_batch)
+            # Format coordinates for OSRM API
+            source_indexes = ";".join(map(str, range(nb_sources)))
+            destination_indexes = ";".join(
+                map(str, range(nb_sources, nb_destinations + nb_sources))
+            )
+            all_coords = source_batch + destination_batch
+            coords_str = ";".join([f"{lon},{lat}" for lat, lon in all_coords])
+
+            # Requesting distance and duration matrix
+            url = f"{OSRM_API_URL}/{coords_str}?sources={source_indexes}&destinations={destination_indexes}&annotations=duration,distance"
+            # url += "&fallback_speed=10"
+
+            logger.info(
+                f"Computing distances and durations for {nb_sources} sources and {nb_destinations} destinations."
+            )
+            response = requests.get(url, timeout=REQUEST_TIMEOUT)
+            response.raise_for_status()
+            data = response.json()
+
+            # Fill the matrices
+            matrix_slice = (slice(i, i + nb_sources), slice(j, j + nb_destinations))
+            distance_matrix[matrix_slice] = np.asarray(data["distances"])
+            duration_matrix[matrix_slice] = np.asarray(data["durations"])
+
+            j += nb_destinations
+        i += nb_sources
+
+    return TableMatrices(distance_matrix, duration_matrix)
+
+
+def compute_distances(df: pd.DataFrame, source_coords: Coordinate) -> pd.DataFrame:
+    """
+    Compute distances and durations from the source coordinate to each event using OSRM API.
+
+    Args:
+        df: The DataFrame containing event coordinates.
+        source_coords: The coordinates of the source.
+
+    Returns:
+        The DataFrame with added 'distance_km' and 'duration_sec' columns.
+    """
+    # Collect coordinates to be sent to OSRM API
+    destination_coords = list(zip(df["latitude"], df["longitude"], strict=False))
+
+    table_matrices = get_table_matrices([source_coords], destination_coords)
+
+    # Extract durations and distances from OSRM response
+    durations: np.ndarray = table_matrices.duration[0]
+    distances: np.ndarray = table_matrices.distance[0]
+
+    def format_duration(s: int) -> str:
+        hours, remainder = divmod(s, 3600)
+        minutes, _ = divmod(remainder, 60)
+        return f"{hours}h {minutes:02d}m"
+
+    # df["Trajet"] = str(timedelta(seconds=durations))
+    df["Trajet"] = [
+        format_duration(int(duration)) if duration is not None else None for duration in durations
+    ]
+    df["Distance"] = [
+        distance / 1000 if distance is not None else None for distance in distances
+    ]  # Convert meters to kilometers
+
     return df
 
 
-def main() -> None:
-    """Scrap events from Cartagrimpe."""
+@logger.catch(reraise=True)
+def main() -> None:  # noqa: PLR0915
+    """Scrap and filter events from Cartagrimpe."""
     logger.remove()
     logger.add(
         "logs/app.log",
-        level="DEBUG",
+        level="TRACE",
         rotation="1 week",
         compression="zip",
     )
@@ -311,11 +439,20 @@ def main() -> None:
     logger.add(
         sys.stdout,
         level="INFO",
+        format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{message}</level>",
     )
 
     # Scrap events
-    events = scrap_events(URL, nb_pages=None)  # Temp: test
+    logger.info("Scrapping events...")
+    start_date = datetime.today().strftime("%Y-%m-%d")
+    events = scrap_events(EVENT_CALENDAR_URL, start_date=start_date, nb_pages=None)
     logger.info(f"Found {len(events)} events")
+
+    # Filter by type
+    filtered_out_types = {EventType.FILM, EventType.GATHERING}
+    events = [event for event in events if event.type not in filtered_out_types]
+    logger.info(f"{len(events)} events remaining after filtering out {filtered_out_types}")
+
     df_events = pd.DataFrame([attrs.asdict(event) for event in events])
 
     # Initialize geolocator
@@ -342,7 +479,7 @@ def main() -> None:
                 logger.error(f"Failed to geocode base location: {base_address}")
                 print(f"Failed to geocode base location: {base_address}")
                 return
-            base_coords = base_location
+            base_coords = base_location.coords
         else:
             # Use detected coordinates as base_coords
             base_coords = current_location
@@ -350,30 +487,56 @@ def main() -> None:
         # If detection failed, prompt the user to enter the address
         base_address = input("Enter your base location address (e.g., 'Paris, France'): ").strip()
         base_location = geocode_address(base_address, geolocator, geocode_cache)
+        # TODO: Print Detailed address found
         if not base_location:
             logger.error(f"Failed to geocode base location: {base_address}")
             print(f"Failed to geocode base location: {base_address}")
             return
-        base_coords = base_location
+        base_coords = base_location.coords
+
+    save_geocode_cache(geocode_cache)
 
     # Compute distances
     logger.debug("Computing distances from base location to events...")
     df_events = compute_distances(df_events, base_coords)
-    save_geocode_cache(geocode_cache)
+
+    # Export full data
+    df_events.to_csv("climbing_events_full.csv", index=False)
 
     # Filter by distance
-    max_distance = float(input("Enter the maximum distance (km) to filter events: "))
+    # max_distance = float(input("Enter the maximum Distance to filter events: ") or 200)
+    max_distance = 200
 
-    # Apply filtering
-    df_filtered = df_events[df_events["distance_km"] <= max_distance].copy()
+    df_filtered = df_events[df_events["Distance"] <= max_distance].copy()
     logger.info(f"Filtered down to {len(df_filtered)} events within {max_distance} km.")
 
     if df_filtered.empty:
         print("No events found within the specified distance.")
         return
 
+    df_filtered = df_events
+
     # Export
     output_file = "climbing_events.csv"
+    df_filtered = df_filtered.rename(
+        columns={
+            Event.date_start.__name__: "Date",  # pyright:ignore[reportAttributeAccessIssue]
+            Event.name.__name__: "Nom",  # pyright:ignore[reportAttributeAccessIssue]
+            Event.url.__name__: "Lien",  # pyright:ignore[reportAttributeAccessIssue]
+            Event.type.__name__: "Type d'évènement",
+            Event.address.__name__: "Adresse",  # pyright:ignore[reportAttributeAccessIssue]
+        }
+    )
+    # Add hyperlink
+    df_filtered["Nom"] = df_filtered.apply(
+        lambda row: f'=HYPERLINK("{row["Lien"]}"; "{row["Nom"]}")', axis=1
+    )
+    df_filtered["Distance"] = df_filtered["Distance"].map(
+        lambda x: f"{int(x)}" if (x is not None and not np.isnan(x)) else ""
+    )
+
+    df_filtered = df_filtered[["Date", "Nom", "Ville", "Adresse", "Distance", "Trajet"]]
+    df_filtered.sort_values("Trajet", inplace=True)
     df_filtered.to_csv(output_file, index=False)
 
     logger.info(f"Exported filtered events to {output_file}.")
